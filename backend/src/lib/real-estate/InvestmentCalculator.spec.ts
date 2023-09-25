a1e6cf5156f32f345d6a6c143896035cdc3b7669
Juha Koivisto <juha.koivisto@schibsted.com>
2023-09-25 20:18:03 +0300

    Stuff

--- backend/src/lib/real-estate/InvestmentCalculator.spec.ts
+++ backend/src/lib/real-estate/InvestmentCalculator.spec.ts
@@ -6,12 +6,33 @@ describe("InvestmentCalculator", () => {
     it("should calculate the correct key figures", () => {
         //Arrange
         let calc = new Lib.RealEstate.InvestmentCalculator({
-            purchasePrice: 100000,
-            deptShare: 2999.79
+            deptFreePrice: 100000,
+            deptShare: 2999.79,
+            transferTaxPercent: 2,
+            maintenanceFee: 66,
+            chargeForFinancialCosts: 19.63,
+            rentPerMonth: 500,
+            apartmentSize: 30,
+            waterCharge: 16
         });
 
-        expect(calc.purchasePrice).toBe(100000);
+        //Input checks
+        expect(calc.deptFreePrice).toBe(100000);
         expect(calc.deptShare).toBe(2999.79);
+        expect(calc.transferTaxPercent).toBe(2);
+        expect(calc.maintenanceFee).toBe(66);
+        expect(calc.chargeForFinancialCosts).toBe(19.63);
+        expect(calc.rentPerMonth).toBe(500);
+        expect(calc.apartmentSize).toBe(30);
+        expect(calc.waterCharge).toBe(16);
+
+        //Calculation checks
+        expect(calc.sellingPrice).toBe(97000.21);
+        expect(calc.transferTax).toBe(2000);
+        expect(calc.maintenanceCosts).toBe(85.63);
+        expect(calc.rentalYieldPercent).toBe(5.11);
+        expect(calc.rentalIncomePerYear).toBe(6000);
+        expect(calc.pricePerSquareMeter).toBe(3333.33);
 
     });
 });
--- backend/src/lib/real-estate/InvestmentCalculator.ts
+++ backend/src/lib/real-estate/InvestmentCalculator.ts
@@ -2,19 +2,69 @@
 export namespace Lib {
     export namespace RealEstate {
         export class InvestmentCalculator {
-            public purchasePrice: number = 0;
-            public deptShare: number = 0;
+            //All properties are public and readonly
+            public readonly deptFreePrice: number = 0;
+            public readonly deptShare: number = 0;
+            public readonly transferTaxPercent: number;
+            public readonly maintenanceFee: number;
+            public readonly chargeForFinancialCosts: number;
+            public readonly rentPerMonth: number;
+            public readonly apartmentSize: number;
+            public readonly waterCharge: number;
 
             constructor(investment: InvestmentInput) {
-                this.purchasePrice = investment.purchasePrice;
+                this.deptFreePrice = investment.deptFreePrice;
                 this.deptShare = investment.deptShare;
+                this.transferTaxPercent = investment.transferTaxPercent;
+                this.maintenanceFee = investment.maintenanceFee;
+                this.chargeForFinancialCosts = investment.chargeForFinancialCosts;
+                this.rentPerMonth = investment.rentPerMonth;
+                this.apartmentSize = investment.apartmentSize || 0;
+                this.waterCharge = investment.waterCharge || 0;
+            }
+
+            public get sellingPrice(): number {
+                return this.roundToTwo(this.deptFreePrice - this.deptShare)
+            }
+
+            public get transferTax(): number {
+                return this.roundToTwo(this.deptFreePrice * this.transferTaxPercent / 100);
+            }
+
+            public get maintenanceCosts(): number {
+                return this.roundToTwo(this.maintenanceFee + this.chargeForFinancialCosts);
+            }
+
+            public get rentalYieldPercent(): number {
+                const rentalYield =
+                    (this.rentPerMonth - this.maintenanceFee) * 12 /
+                    (this.deptFreePrice + this.transferTax) * 100;
+                return this.roundToTwo(rentalYield);
+            }
+
+            public get rentalIncomePerYear(): number {
+                return this.roundToTwo(this.rentPerMonth * 12);
+            }
+
+            public get pricePerSquareMeter(): number {
+                return this.roundToTwo(this.deptFreePrice / this.apartmentSize);
+            }
+
+            private roundToTwo(num: number): number {
+                return Math.round(num * 100) / 100;
             }
         }
 
         //Investment input type
         export type InvestmentInput = {
-            purchasePrice: number;
+            deptFreePrice: number;
             deptShare: number;
+            transferTaxPercent: number;
+            maintenanceFee: number;
+            chargeForFinancialCosts: number;
+            rentPerMonth: number;
+            apartmentSize?: number;
+            waterCharge?: number;
         }
     }
 }
