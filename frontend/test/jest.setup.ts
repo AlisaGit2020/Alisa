48319ec493dc3ab6360b509e4d3ce2f7d0333f6a
Juha Koivisto <juha.koivisto@m10s.io>
2026-02-05 10:24:12 +0200

    feat: configure MSW server in Jest setup

--- frontend/test/jest.setup.ts
+++ frontend/test/jest.setup.ts
@@ -1,2 +1,13 @@
-require ('reflect-metadata');
+// frontend/test/jest.setup.ts
+require('reflect-metadata');
+import { server } from './msw/server';
+
+// Establish API mocking before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
+
+// Reset any request handlers that are added during tests
+afterEach(() => server.resetHandlers());
+
+// Clean up after tests are finished
+afterAll(() => server.close());
 
